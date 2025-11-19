5a90d80d5165c910537e18b42697e8f245325c49
Vivek Singh <vviveksingh8874@gmail.com>
2025-11-19 12:27:29 +0530

    cors code added

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
-import express, {json} from "express";
+import express, { json } from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/connectDB.js";
 import cors from "cors";
-import TodoRoute from "./routes/todoRoutes.js"
+import TodoRoute from "./routes/todoRoutes.js";
 
 const app = express();
 dotenv.config();
 connectDB();
-const PORT = process.env.PORT; 
+const PORT = process.env.PORT;
 
-app.get("/" , (req, res) => {
-    res.send("server is running");
-});
+// CORS configuration
+const allowedOrigins = [
+  "https://vivek.code4bharat.com",
+];
 
-//middleware
-app.use(cors());
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true,
+}));
+
 app.use(json());
 
-//routes
+// Test Route
+app.get("/", (req, res) => {
+  res.send("server is running");
+});
+
+// Routes
 app.use("/api/todos", TodoRoute);
 
 app.listen(PORT, () => {
-    console.log(`app is listening at port ${PORT}`)
-})
+  console.log(`app is listening at port ${PORT}`);
+});
